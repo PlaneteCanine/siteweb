015e01b7901d59e8d7547a302b31a6fd241f648c
Nima <50780747+JacksonMelloGang@users.noreply.github.com>
2021-12-14 23:11:11 +0100

    gave up loading screen focusing on css and shop.html

--- js/loadingscreen.js
+++ js/loadingscreen.js
@@ -1,13 +1,31 @@
 function loadingscreen(){
     var div = document.createElement("div");
     div.className = "loading";
-    div.style.width = "1000px";
-    div.style.margin = "auto";
-    div.style.backgroundColor = "blue";
+    div.style.width = "100%";
+    div.style.height = "100%";
+    div.style.top = "0px";
+    div.style.left = "0px";
     div.style.position = "absolute";
-    div.style.height = "100px";
-    div.style.zIndex = 1;
+    div.style.backgroundColor = "#663b1e";
+    div.style.zIndex = 10;
+    div.style.textAlign = "center";
+    
+    
 
-    document.body.appendChild(div);
-    console.log("aaaa");
+    var img = document.createElement("img");
+    img.src = "../img/loadingscreen/dog foot.png";
+    img.height = 50;
+    img.width = 50;
+    img.style.padding = "70px";
+    img.style.position = "absolute";
+    img.style.zIndex = '15';
+
+    console.log("Starting the loading screen loop..");
+    while(document.readyState != "complete"){
+        console.log("Playing animation");
+    }
+    
+
+    //document.body.appendChild(div);
+    //document.body.getElementsByClassName("loading")[0].appendChild(img);
 }
